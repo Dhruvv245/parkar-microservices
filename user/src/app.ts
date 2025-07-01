44875dcd8b7e9454188a4bb0b12bac49eddae54c
Dhruv Chauhan <chauhandhruv245@gmail.com>
2025-07-01 23:28:43 +0530

    global middleware for parsing

--- user/src/app.ts
+++ user/src/app.ts
@@ -1,7 +1,14 @@
-import express, { Express} from "express";
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import cookieParser from "cookie-parser";
 import userRouter from './routes/user.routes';
 
+dotenv.config({path:'./config.env'});
 const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/user',userRouter);
 
